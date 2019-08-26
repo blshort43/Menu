f8fda0bef41cc6b04821e2fb9cf23e9a4f341585
Brandon <blshort43@users.noreply.github.com>
2019-08-26 17:04:14 -0500

    added additional props

--- index.js
+++ index.js
@@ -4,92 +4,105 @@ import styled from 'styled-components';
 import { Button, Card, Flex } from 'components';
 
 const MenuContainer = styled.div`
-  position: relative;
-  width: fit-content;
-  overflow: visible;
+   position: relative;
+   float: ${props => (props.float ? props.float : null)};
+   width: fit-content;
+   overflow: visible;
 `;
 
 const MenuButton = styled(Button)`
-  cursor: pointer;
-  outline: none;
+   cursor: pointer;
+   outline: none;
 `;
 
 const MenuListContainer = styled(Card)`
-  position: absolute;
-  top: 8px;
-  right: ${props => (props.listLeft ? '8px' : null)};
-  left: ${props => (props.listRight ? '8px' : null)};
+   position: absolute;
+   top: 8px;
+   right: ${props => (props.listLeft ? '8px' : null)};
+   left: ${props => (props.listLeft ? null : '8px')};
 `;
 
 const MenuList = styled(Flex)`
-  padding-top: 8px;
-  padding-bottom: 8px;
-  flex-direction: column;
-  width: max-content;
+   padding-top: 8px;
+   padding-bottom: 8px;
+   flex-direction: column;
+   width: max-content;
 `;
 
 export const Menu = ({
-  label,
-  children,
-  icon,
-  listLeft,
-  listRight,
-  ...props
+   float,
+   label,
+   children,
+   color,
+   icon,
+   listBackground,
+   listLeft,
+   bg,
+   ...props
 }) => {
-  const node = useRef();
+   const node = useRef();
 
-  const [open, setOpen] = useState(false);
+   const [open, setOpen] = useState(false);
 
-  useEffect(() => {
-    // add when mounted
-    document.addEventListener('mousedown', handleClick);
-    // return function to be called when unmounted
-    return () => {
-      document.removeEventListener('mousedown', handleClick);
-    };
-  }, []);
+   useEffect(() => {
+      // add when mounted
+      document.addEventListener('mousedown', handleClick);
+      // return function to be called when unmounted
+      return () => {
+         document.removeEventListener('mousedown', handleClick);
+      };
+   }, []);
 
-  const handleClick = e => {
-    if (node.current.contains(e.target)) {
-      // inside click
-      return;
-    }
-    // outside click
-    setOpen(false);
-  };
+   const handleClick = e => {
+      if (node.current.contains(e.target)) {
+         // inside click
+         return;
+      }
+      // outside click
+      setOpen(false);
+   };
 
-  const handleChange = () => {
-    setOpen(false);
-  };
+   const handleChange = () => {
+      setOpen(false);
+   };
 
-  return (
-    <MenuContainer ref={node}>
-      <MenuButton
-        {...props}
-        onFocus={() => setOpen(!open)}
-        icon={icon}
-        label={label}
-      >
-        {(icon &&
-          React.cloneElement(icon, {
-            size: icon.props.size || 30,
-          })) ||
-          label}
-      </MenuButton>
-      {open === true ? (
-        <MenuListContainer listLeft={listLeft} listRight={listRight}>
-          <MenuList onClick={e => handleChange(e)}>{children}</MenuList>
-        </MenuListContainer>
-      ) : null}
-    </MenuContainer>
-  );
+   return (
+      <MenuContainer float={float} ref={node}>
+         <MenuButton
+            {...props}
+            bg={bg}
+            color={color}
+            onClick={() => setOpen(!open)}
+            icon={icon}
+            label={label}
+         >
+            {(icon &&
+               React.cloneElement(icon, {
+                  size: icon.props.size || 30
+               })) ||
+               label}
+         </MenuButton>
+         {open === true ? (
+            <MenuListContainer
+               {...props}
+               bg={listBackground || 'paper'}
+               listLeft={float === 'right' || listLeft}
+            >
+               <MenuList onClick={e => handleChange(e)}>{children}</MenuList>
+            </MenuListContainer>
+         ) : null}
+      </MenuContainer>
+   );
 };
 Menu.propTypes = {
-  label: PropTypes.string,
-  children: PropTypes.any,
-  icon: PropTypes.any,
-  listLeft: PropTypes.bool,
-  listRight: PropTypes.bool,
+   bg: PropTypes.string,
+   color: PropTypes.string,
+   float: PropTypes.string,
+   label: PropTypes.string,
+   children: PropTypes.any,
+   icon: PropTypes.any,
+   listBackground: PropTypes.string,
+   listLeft: PropTypes.bool
 };
 
 export default Menu;
