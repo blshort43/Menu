62da03f5f86dff64491cfb25bd24e01e1a7b3e59
Brandon <blshort43@users.noreply.github.com>
2019-08-28 08:32:00 -0500

    added fade in/out to menu
    added fade in/out to menu

--- index.js
+++ index.js
@@ -4,106 +4,110 @@ import styled from 'styled-components';
 import { Button, Card, Flex } from 'components';
 
 const MenuContainer = styled.div`
-   position: relative;
-   width: fit-content;
-   overflow: visible;
+  position: relative;
+  width: fit-content;
+  overflow: visible;
 `;
 
 const MenuButton = styled(Button)`
-   cursor: pointer;
-   outline: none;
+  cursor: pointer;
+  outline: none;
 `;
 
 const MenuListContainer = styled(Card)`
-   position: absolute;
-   top: 8px;
-   right: ${props => (props.listLeft ? '8px' : null)};
-   left: ${props => (props.listLeft ? null : '8px')};
+  position: absolute;
+  top: 8px;
+  right: ${props => (props.listLeft ? '8px' : null)};
+  left: ${props => (props.listLeft ? null : '8px')};
+  opacity: ${props => (props.fadeInOut ? 1 : 0)};
+  visibility: ${props => (props.fadeInOut ? 'visible' : 'hidden')};
+  transition: all 0.1s linear;
 `;
 
 const MenuList = styled(Flex)`
-   padding-top: 8px;
-   padding-bottom: 8px;
-   flex-direction: column;
-   width: max-content;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  flex-direction: column;
+  width: max-content;
 `;
 
 export const Menu = ({
-   bg,
-   float,
-   label,
-   children,
-   color,
-   icon,
-   listBackground,
-   listLeft,
-   style,
-   ...props
+  bg,
+  float,
+  label,
+  children,
+  color,
+  icon,
+  listBackground,
+  listLeft,
+  style,
+  ...props
 }) => {
-   const node = useRef();
+  const node = useRef();
 
-   const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-   useEffect(() => {
-      // add when mounted
-      document.addEventListener('mousedown', handleClick);
-      // return function to be called when unmounted
-      return () => {
-         document.removeEventListener('mousedown', handleClick);
-      };
-   }, []);
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener('mousedown', handleClick);
+    // return function to be called when unmounted
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, []);
 
-   const handleClick = e => {
-      if (node.current.contains(e.target)) {
-         // inside click
-         return;
-      }
-      // outside click
-      setOpen(false);
-   };
+  const handleClick = e => {
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
+    }
+    // outside click
+    setOpen(false);
+  };
 
-   const handleChange = () => {
-      setOpen(false);
-   };
+  const handleChange = () => {
+    setOpen(false);
+  };
 
-   return (
-      <MenuContainer style={style} ref={node}>
-         <MenuButton
-            {...props}
-            bg={bg}
-            color={color}
-            onClick={() => setOpen(!open)}
-            icon={icon}
-            label={label}
-         >
-            {(icon &&
-               React.cloneElement(icon, {
-                  size: icon.props.size || 30
-               })) ||
-               label}
-         </MenuButton>
-         {open === true ? (
-            <MenuListContainer
-               {...props}
-               bg={listBackground || 'paper'}
-               listLeft={(style && style.float === 'right') || listLeft}
-            >
-               <MenuList onClick={e => handleChange(e)}>{children}</MenuList>
-            </MenuListContainer>
-         ) : null}
-      </MenuContainer>
-   );
+  return (
+    <MenuContainer style={style} ref={node}>
+      <MenuButton
+        {...props}
+        bg={bg}
+        color={color}
+        onClick={() => setOpen(!open)}
+        icon={icon}
+        label={label}
+      >
+        {(icon &&
+          React.cloneElement(icon, {
+            size: icon.props.size || 30,
+          })) ||
+          label}
+      </MenuButton>
+
+      <MenuListContainer
+        {...props}
+        onClick={e => handleChange(e)}
+        fadeInOut={open}
+        bg={listBackground || 'paper'}
+        listLeft={(style && style.float === 'right') || listLeft}
+      >
+        <MenuList onClick={e => handleChange(e)}>{children}</MenuList>
+      </MenuListContainer>
+    </MenuContainer>
+  );
 };
 Menu.propTypes = {
-   bg: PropTypes.string,
-   color: PropTypes.string,
-   float: PropTypes.string,
-   label: PropTypes.string,
-   children: PropTypes.any,
-   icon: PropTypes.any,
-   listBackground: PropTypes.string,
-   listLeft: PropTypes.bool,
-   style: PropTypes.object
+  bg: PropTypes.string,
+  color: PropTypes.string,
+  float: PropTypes.string,
+  label: PropTypes.string,
+  children: PropTypes.any,
+  icon: PropTypes.any,
+  listBackground: PropTypes.string,
+  listLeft: PropTypes.bool,
+  style: PropTypes.object,
 };
 
 export default Menu;
